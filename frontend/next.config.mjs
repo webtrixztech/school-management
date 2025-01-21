6db96031074ce6cc629f11c6c1a97927f7f1d7dc
webtrixztech <hamidaliwebtrixz@gmail.com>
2025-01-21 13:11:29 +0530

    adD

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        HOSTNAME: 'localhost',
-        APIBASEURL: 'http://localhost:5000',
-      },
+  env: {
+    HOSTNAME: 'localhost',
+    APIBASEURL: 'http://localhost:5000',
+  },
+  experimental: {
+    appDir: true, // Enable app directory for routing
+  },
 };
 
 export default nextConfig;
