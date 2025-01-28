4558f3ec7837304ad5a9b2fee6d0e326e144e034
webtrixztech <hamidaliwebtrixz@gmail.com>
2025-01-28 16:05:04 +0530

    add

--- backend/controllers/schoolControlller.js
+++ backend/controllers/schoolControlller.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const ZOHO_ACCOUNT_ID = process.env.ZOHO_ACCOUNT_ID;
-const ZOHO_REFRESH_TOKEN = "1000.300b021bb66e2523f85d0c86d5c39f69.66b730fd15557baacfd5ae05bc2bc296";
+const ZOHO_REFRESH_TOKEN = "1000.cefe6cd455a4e149ca234c1535c84dbb.d6d37e81d8ac777c0b15a5078168cf21";
 const ZOHO_CLIENT_ID = "1000.FTS6LQWHAI6VCHP67WSH71HZBW091I";
 const ZOHO_CLIENT_SECRET = "355528be04b59595a5f5c471f732212ee77d5b6e52";
 const ZOHO_PAYMENT_API = "https://payments.zoho.in/api/v1/paymentsessions?account_id=60034736461";
